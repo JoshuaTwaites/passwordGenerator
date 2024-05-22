2573c62521333921c372ab37d71182b7c767480c
JoshuaTwaites <joshuatwaites5@gmail.com>
2024-05-23 00:16:38 +0100

    first commit

---
+++ passwordGenerator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+    char characters[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!£$%&*";
+    string password;
+    int length, randomNum;
+
+    ofstream out_file;
+
+    out_file.open("Password.txt");
+
+    cout << "Enter the number of digits you would like the password to be: ";
+    cin >> length;
+
+    for (int i = 1; i <= length; i++)
+    {
+        randomNum = rand() % 68 + 1;
+
+        out_file << characters[randomNum];
+    }
+
+    cout << "Done!" << endl;
+
+    return 0;
+}
